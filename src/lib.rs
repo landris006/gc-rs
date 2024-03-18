5f959ae9fbbedc5d5fb8124a0397860d4c5c8bfe
landris006 <laczko.andris007@gmail.com>
2024-03-18 21:44:45 +0100

    cleaned up main

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 mod handlers;
+use std::path::PathBuf;
+
+use clap::{arg, value_parser, Arg, Command};
 
 use anyhow::Result;
 use calendar3::{
@@ -10,6 +13,46 @@ use calendar3::{
 use clap::ArgMatches;
 use google_calendar3 as calendar3;
 use std::{env, ffi::OsStr, path::Path};
+
+pub fn get_matches() -> clap::ArgMatches {
+    clap::command!()
+        .subcommand(
+            Command::new("setup")
+                .arg(Arg::new("file").value_name("FILE").value_parser(
+                    value_parser!(PathBuf)
+                ).required(true).help("The path to the credentials file"))
+                .about("Copies your Google credentials file to the data directory")
+        )
+        .subcommand(
+            Command::new("calendars")
+                .about("List avaliable calendars")
+                .arg(arg!(--id "Display the IDs of the calendars")),
+        )
+        .subcommand(Command::new("logout").about("Delete the cached access tokens"))
+        .subcommand(
+            Command::new("meet")
+                .about("Open Google Meet link")
+                .arg(
+                    Arg::new("event-index")
+                        .value_name("INDEX")
+                        .value_parser(value_parser!(usize))
+                        .help("Optionally specify the index of the event to open (defaults to the ongoing or next event)")
+                )
+                .arg(
+                    Arg::new("calendar-id")
+                        .long("calendar-id")
+                        .default_value("primary")
+                        .help("Specify the ID of the calendar to use"),
+                )
+        )
+        .arg(
+            Arg::new("calendar-id")
+                .long("calendar-id")
+                .default_value("primary")
+                .help("Specify the ID of the calendar to use"),
+        )
+        .get_matches()
+}
 
 fn bin_name() -> Option<String> {
     env::current_exe()
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,7 @@
-use std::path::PathBuf;
-
 use anyhow::Result;
-use clap::{arg, value_parser, Arg, Command};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = clap::command!()
-        .subcommand(
-            Command::new("setup")
-                .arg(Arg::new("file").value_name("FILE").value_parser(
-                    value_parser!(PathBuf)
-                ).required(true).help("The path to the credentials file"))
-                .about("Copies your Google credentials file to the data directory")
-        )
-        .subcommand(
-            Command::new("calendars")
-                .about("List avaliable calendars")
-                .arg(arg!(--id "Display the IDs of the calendars")),
-        )
-        .subcommand(Command::new("logout").about("Delete the cached access tokens"))
-        .subcommand(
-            Command::new("meet")
-                .about("Open Google Meet link")
-                .arg(
-                    Arg::new("event-index")
-                        .value_name("INDEX")
-                        .value_parser(value_parser!(usize))
-                        .help("Optionally specify the index of the event to open (defaults to the ongoing or next event)")
-                )
-                .arg(
-                    Arg::new("calendar-id")
-                        .long("calendar-id")
-                        .default_value("primary")
-                        .help("Specify the ID of the calendar to use"),
-                )
-        )
-        .arg(
-            Arg::new("calendar-id")
-                .long("calendar-id")
-                .default_value("primary")
-                .help("Specify the ID of the calendar to use"),
-        )
-        .get_matches();
-
+    let matches = gc_rs::get_matches();
     gc_rs::handle(matches).await
 }
